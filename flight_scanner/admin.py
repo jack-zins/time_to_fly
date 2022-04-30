c86b8ed3105c13e08728126556acf87320b59171
Matthew Amszynski <mattames34@gmail.com>
2022-04-29 20:44:12 -0400

    Admin added
    In Lines Created

--- flight_scanner/admin.py
+++ flight_scanner/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from flight_scanner.models import FlightAlertRequest,FlightAlertOrigin,FlightAlertDestination
 # Register your models here.
+
+
+
+class FAOInLine(admin.TabularInline):
+    model = FlightAlertOrigin
+
+class FADinLine(admin.TabularInline):
+    model = FlightAlertDestination
+
+@admin.register(FlightAlertRequest)
+class FlightAlertRequestAdmin(admin.ModelAdmin):
+    inlines = [FADinLine,FAOInLine]
+
