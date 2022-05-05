fcb0b167da04940e4719986c9d45912dcf90c286
Matthew-Amszynski <101362030+Matthew-Amszynski@users.noreply.github.com>
2022-05-05 04:25:32 -0400

    flight_alert_services.py was created (#6)
    search_flights was created

---
+++ flight_scanner/services/flight_alert_services.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from flight_scanner.models import FlightAlertRequest,FlightAlertOrigin,FlightAlertDestination
+import requests
+
+
+def get_origin_string(far:FlightAlertRequest):
+    joins = []
+    for fao in far.flightalertorigin_set.all():
+        joins.append(fao.airport_code)
+    return ','.join(joins)
+
+
+def get_destination_string(far:FlightAlertRequest):
+    joins = []
+    for fad in far.flightalertdestination_set.all():
+        destination_type = fad.destination_type.lower()
+        joins.append(f'{destination_type}:{fad.destination_name}')
+    return ','.join(joins)
+
+
+def get_stopover_to_string(far:FlightAlertRequest):
+    minutes, seconds = divmod(far.layover_duration_limit.total_seconds(), 60)
+    hours, minutes = divmod(minutes, 60)
+    return f"{int(hours):02}:{int(minutes):02}"
+
+
+def search_flights(far: FlightAlertRequest):
+    params = {
+        'date_from': far.start_date,
+        'date_to': far.end_date,
+        'max_stopover': far.layover_limit,
+        'stopover_to' : get_stopover_to_string(far),
+        'fly_from': get_origin_string(far),
+        'fly_to' : get_destination_string(far),
+        'nights_in_dst_from' : far.min_days_at_destination,
+        'nights_in_dst_to' : far.max_days_at_destination,
+        'adults' : far.adults,
+    }
+    header = {"apikey": settings.KIWI_API_KEY}
+    url = 'https://tequila-api.kiwi.com/v2/search'
+    responce = requests.get(url,params=params,headers=header)
+    return responce.json()
+
